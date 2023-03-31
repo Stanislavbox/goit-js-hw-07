d4a5c3c77e9b9e64e97840601792d4458155aec6
Utilizator <Stanislavbox24@gmail.com>
2023-03-31 18:33:39 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+// ? CREAT MARKUP
+const markupItemEl = galleryItems.map(({preview, original, description}) =>
+       `<li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+         <img
+           class="gallery__image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+         />
+       </a>
+     </li>`).join('');
+
+// ? ADD MARKUP IN HTML
+listEl.insertAdjacentHTML('beforeend', markupItemEl)
+
+// ? CREAT A LISTENER
+listEl.addEventListener('click', onClick);
+
+// ? CALLBACK LISTENER FUNCTION
+function onClick(evt){
+      evt.preventDefault();
+      if (!evt.target.classList.contains('gallery__image')){
+            return;
+      }
+      const instance = basicLightbox.create(`
+      <img class="gallery__image" src="${evt.target.dataset.source}" alt="${evt.target.description}"/>
+      `)
+instance.show()
+}
